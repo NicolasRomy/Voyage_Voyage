c39b51e25e85f6247e04d5a25b50695a65c44ff7
unknown <ecros@gamingcampus.local>
2021-03-30 18:21:28 +0200

    pdf final

---
+++ action/PDF.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    $_SESSION;
+    
+    require_once "config.php";
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    $sql = "SELECT * FROM article WHERE id = ". $_Get["id"]."";
+    $pre = $pdo->prepare($sql);
+    $pre->execute();
+    $article = current($pre->fetchAll(PDO::FETCH_ASSOC));
+
+    $mpdf = new \Mpdf\Mpdf();
+    $image = $article["Image"];
+
+    $data = "";
+    $data .=  $article["Title"] . "<br>";
+    $data .=  "<img src = $image> <br>";
+    $data .=  $article["content"] . "<br>";
+    $data .=  $article["Publiacation_date"] . "<br>";
+    $data .=  $article["Article_content_date"] . "<br>";;
+
+    $mpdf->WriteHtml($data);
+    $mpdf->output("myfile.pdf","D");   
+
+
+
+
+?>
