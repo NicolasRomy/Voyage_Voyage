7d36960f7d45a7871aa984f80afd2f379fa419a9
NicolasRomy <nromy@gaming.tech>
2021-03-31 02:00:24 +0200

    blanc

--- reading.php
+++ reading.php
@@ -3,6 +3,13 @@
   $_SESSION;
 
   require_once "action/config.php";
+  $CountryID =  $_POST["country"]
 ?>
-Welcome <?php echo $_POST["country"]; ?><br>
-
+ <?php  $sql= "SELECT * FROM articles WHERE Country_ID =:Country_ID";
+            $databind = array(":Country_ID" => $CountryID);
+            $pre = $pdo->prepare($sql);
+            $pre->execute($databind);
+            $article = $pre->fetchAll(PDO::FETCH_ASSOC);
+            
+ ?>
+ <h1><?php echo $article[0]['Title']?></h1>
